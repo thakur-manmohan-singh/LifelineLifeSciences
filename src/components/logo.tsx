93f362353626feea8d7b2f92a4223c949ce610d2
Manmohan Singh <manmohansingh10032004@gmail.com>
2025-09-23 11:06:58 +0000

    make logo bigger in header

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export function Logo() {
   return (
     <a href="/" className="flex items-center" aria-label="Lifeline Life Sciences Homepage">
-      <Image src="/images/logo.png" alt="Lifeline Life Sciences Logo" width={56} height={56} className="h-14 w-14" />
+      <Image src="/images/logo.png" alt="Lifeline Life Sciences Logo" width={64} height={64} className="h-16 w-16" />
     </a>
   );
 }
