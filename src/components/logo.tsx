31128338062551342dbcbc47e1f54a840697bdca
Manmohan Singh <manmohansingh10032004@gmail.com>
2025-09-23 11:10:05 +0000

    more bigger and not same height and width , its logo and name so keep th

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export function Logo() {
   return (
     <a href="/" className="flex items-center" aria-label="Lifeline Life Sciences Homepage">
-      <Image src="/images/logo.png" alt="Lifeline Life Sciences Logo" width={64} height={64} className="h-16 w-16" />
+      <Image src="/images/logo.png" alt="Lifeline Life Sciences Logo" width={160} height={80} className="h-20 w-40" />
     </a>
   );
 }
