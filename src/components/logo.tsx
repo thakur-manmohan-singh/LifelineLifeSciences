7584fa4f178141f9e0bee6796423a3c51b73428b
Manmohan Singh <manmohansingh10032004@gmail.com>
2025-09-23 09:49:27 +0000

    images are not visible in entire website most of the places , fix the pa

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -6,11 +6,11 @@ export function Logo() {
       <Image
         src="/images/logo.png"
         alt="Advanced Care Logo"
-        width={180}
+        width={40}
         height={40}
         priority
       />
-      <span className="hidden sm:inline-block font-headline text-xl font-bold text-foreground">Lifeline Life Sciences</span>
+      <span className="sm:inline-block font-headline text-xl font-bold text-foreground hidden">Lifeline Life Sciences</span>
     </a>
   );
 }
